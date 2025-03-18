0853a3d7a3f7812b93119e41231bb9e11572f86c
andre-axt <andreaxtbachtold@gmail.com>
2025-03-18 10:07:12 -0300

    edit

---
+++ script.js
@@ -0,0 +1,114 @@
+console.log("Hello World");
+
+let nome = "andre";
+const idade = 17;
+
+function saudacao() {
+    console.log("Olá, " + nome);
+}
+
+saudacao()
+
+if (idade >= 18) {
+    console.log()
+}
+
+function verificaPar(){
+    let numero = 2;
+    if (numero % 2 === 0){
+        console.log("é par");
+    }
+    else{
+        console.log("é impar");
+    }
+}
+
+verificaPar()
+
+function calcular(){
+    let numero1 = 2;
+    let numero2 = 3;
+    console.log(numero1 + numero2);
+    console.log(numero1 - numero2);
+    console.log(numero1 * numero2);
+    console.log(numero1 / numero2);
+}
+
+calcular()
+
+i = 10;
+while (i >= 1){
+    console.log(i)
+    i--;
+} 
+
+function inverterTexto(str){
+    let solucao = "";
+    let tamanho = str.length-1;
+    for(let posicao = tamanho; posicao >= 0; posicao--){
+        solucao = solucao + str[posicao];
+    }
+    console.log(solucao);
+    return solucao;
+}
+inverterTexto("javascript")
+
+let palavra = "pastel"
+function CountCaracter(str) {
+    print(str.length)
+}
+CountCaracter(palavra)
+
+
+let carro = {
+    marca: "toyota",
+    modelo: "supra",
+    ano: "2008",
+    modeloMostrar: function(){
+        console.log(this.modelo)
+    }
+
+};
+carro.modeloMostrar();
+
+function mensagemPersonalizada() {
+    let nome = "andre"
+    let mensagem = "olá"
+
+    console.log(mensagem, nome)
+}
+mensagemPersonalizada()
+
+function media(){
+    let num1 = 1
+    let num2 = 2
+    let num3 = 3
+    let media = (num1 + num2 + num3)/3
+    console.log(media)
+}
+media()
+
+for (let i = 1; i < 21; i++) {
+    if(i % 3 === 0){
+        console.log(i, "é multiplo de 3");
+    }
+    else{
+        console.log(i);
+    }
+}
+
+let palavra2 = "arara"
+function verificarPalindromo(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    if (palavra2 == joinArray) {
+        print("Palindromo")
+    } else {
+        print("Não é palindromo")
+    }
+}
+verificarPalindromo(palavra2)
+
+
+
